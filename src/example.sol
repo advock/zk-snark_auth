d62b336cce81188625b602486c02052933450830
ADVK <advaiyakarnik@gmail.com>
2023-04-23 22:31:26 +0530

    example contract added

---
+++ src/example.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+interface IVerifier {
+    function verifiyProof(
+        bytes memory _proof,
+        uint256[6] memory _input
+    ) external returns (bool);
+}
+
+contract example {
+    IVerifier public immutable verifier;
+    mapping(address => bool) public allowed;
+
+    constructor(IVerifier _verifier) {
+        verifier = _verifier;
+    }
+
+    function verifyUser() public {
+        require(!allowed[msg.sender], "already veridied ");
+    }
+}
